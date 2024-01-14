b1818d9a8791dd80fd4ef80cd94886fa4fd1b6f8
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2024-01-13 22:05:26 -0600

    add initial files

---
+++ lib/POE/Wheel/Run/DaemonHelper.pm
@@ -0,0 +1,105 @@
+package POE::Wheel::Run::DaemonHelper;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+POE::Wheel::Run::DaemonHelper - The great new POE::Wheel::Run::DaemonHelper!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use POE::Wheel::Run::DaemonHelper;
+
+    my $foo = POE::Wheel::Run::DaemonHelper->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-poe-wheel-run-daemonhelper at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=POE-Wheel-Run-DaemonHelper>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc POE::Wheel::Run::DaemonHelper
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=POE-Wheel-Run-DaemonHelper>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/POE-Wheel-Run-DaemonHelper>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/POE-Wheel-Run-DaemonHelper>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2024 by Zane C. Bowers-Hadley.
+
+This is free software, licensed under:
+
+  The GNU Lesser General Public License, Version 2.1, February 1999
+
+
+=cut
+
+1; # End of POE::Wheel::Run::DaemonHelper
